e0ef1ca067b46e0703dd640b58da78059961a384
matheusluna <matheusluna96@gmail.com>
2018-05-22 07:28:20 -0300

    adicionando arquivos

---
+++ index.js
@@ -0,0 +1,24 @@
+butao.onclick = function() {
+  var txt = texto.value;
+  txt = txt.replace("\n", " ");
+  txt = txt.replace(/\.|\,|\(|\)|\?|\:|\; /g, "");
+  var palavras = txt.split(" ");
+  var lista = [];
+  for (var a = 0; a < palavras.length; a++) {
+    var palavra = palavras[a].toString();
+    var lstp = palavra.split(" ");
+    lista.push(lstp);
+  }
+
+
+
+  for (var i = 0; i < lista.length; i++) {
+    if (lista[i] == " ") {
+      document.getElementById("imprime").innerHTML = document.getElementById("imprime").innerHTML + lista[i];
+    }else {
+      document.getElementById("imprime").innerHTML = document.getElementById("imprime").innerHTML + "<br>" + lista[i];
+    }
+
+  }
+
+}
